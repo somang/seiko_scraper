7e0c2ba50fcb658efbfbd44111a482b1e62c81b4
somang <somang.nam@gmail.com>
2022-07-02 01:56:39 -0400

    todo: sqlite insertion set

--- seiko_scraper.py
+++ seiko_scraper.py
@@ -6,11 +6,49 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys 
 from selenium.common.exceptions import NoSuchElementException        
-import urllib
+import urllib.request
 import pandas as pd
+import base64
+import io
+from PIL import Image 
 from bs4 import BeautifulSoup
 import pprint
 import time
+import sqlite3
+
+
+def init_db():
+    # Connecting to sqlite
+    # connection object
+    connection_obj = sqlite3.connect('watches.db')    
+    # cursor object
+    cursor_obj = connection_obj.cursor()    
+    # Drop the GEEK table if already exists.
+    cursor_obj.execute("DROP TABLE IF EXISTS WATCHES")    
+    # Creating table
+    table = """ CREATE TABLE WATCHES (
+                brand TEXT NOT NULL,
+                model TEXT,
+                weight TEXT,
+                water_resistance TEXT,
+                lug_to_lug TEXT,
+                jewel_count TEXT,
+                diameter REAL,
+                thickness REAL
+                lug_width TEXT,
+                crystal TEXT,
+                lume TEXT,
+                case_material TEXT,
+                power_reserve TEXT,
+                mrsp TEXT,
+                movement_name TEXT,
+                movement_type TEXT,
+            ); """    
+    cursor_obj.execute(table)    
+    print("Table is Ready")    
+    # Close the connection
+    connection_obj.close()
+
 
 def check_exists(driver, css_selector):
     try:
@@ -20,7 +58,13 @@ def check_exists(driver, css_selector):
     return True
 
 
-def handle_spec_element(soup, price):
+def print_watch_info(watch_info_dict):
+    for k in watch_info_dict:
+        if k is not 'img':
+            print(f'{k}: {watch_info_dict[k]}')
+
+
+def handle_spec_element(soup, price, pic_blob):
     dlTag = soup.find_all("dl", {"class":"_list"})
     dtdds = []
     for tag in dlTag:
@@ -30,10 +74,20 @@ def handle_spec_element(soup, price):
             strip_keys = keys[i].text.strip()
             strip_vals = list(filter(None, [x.strip() for x in vals[i].text.split('\n')]))
             dtdds.append((strip_keys, strip_vals))
-    info_collection = {'price': price}
+    
+    info_collection = {'MSRP': price, 'img': pic_blob}    
+
     for j in dtdds:
-        info_collection[j[0]] = j[1]    
-    pprint.pprint(info_collection)
+        if isinstance(j[1], list):
+            if len(j[1]) > 1:
+                for c in j[1]:
+                    if ":" in c:
+                        info_collection[c.split(":")[0]] = c.split(":")[1]
+            else:
+                info_collection[j[0]] = j[1][0]
+        else:
+            info_collection[j[0]] = j[1]
+    # pprint.pprint(info_collection)
     return info_collection
     
 
@@ -72,13 +126,17 @@ def find_element_click(driver, by, expression, search_window=None, timeout=32, i
     return False
 
 
-# def save_img():
-#     # get the image source
-#     img = driver.find_element_by_xpath('//div[@id="recaptcha_image"]/img')
-#     src = img.get_attribute('src')
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
 
-#     # download the image
-#     urllib.urlretrieve(src, "captcha.png")
+
+def write_file(data, filename):
+    # Convert binary data to proper format and write it on Hard Disk
+    with open(filename, 'wb') as file:
+        file.write(data)
 
 
 def main():
@@ -87,34 +145,34 @@ def main():
     driver.get("https://www.seikowatches.com/ca-en/watchfinder?page=1")
     watchlist_div_selector = '#app > div > div.watchfinder-result > div > div.watchfinder-list > div.row.row-cols-2.row-cols-md-4.gy-1.gy-md-3.gx-1.gx-md-3'
     button_selector = '#app > div > div.watchfinder-result > div > div.watchfinder-list > div.watchfinder-list-more > button'
+    img_selector = 'body > main > div > div > div.productInformation > div.productInformation-main > div.productInformation-visual > div > div._visual > div > figure > a > img'
     watch_info = {} # to store all watch info
     timeout = 5
     try:
-        element_present = EC.presence_of_element_located((By.CSS_SELECTOR, watchlist_div_selector))
-        WebDriverWait(driver, timeout).until(element_present)
+        watchlist_div = EC.presence_of_element_located((By.CSS_SELECTOR, watchlist_div_selector))
+        WebDriverWait(driver, timeout).until(watchlist_div)
 
-        element_present = EC.presence_of_element_located((By.CSS_SELECTOR, button_selector))
-        WebDriverWait(driver, timeout).until(element_present)
+        load_button = EC.presence_of_element_located((By.CSS_SELECTOR, button_selector))
+        WebDriverWait(driver, timeout).until(load_button)
 
-        # while check_exists(driver, button_selector):  
-        #     # Get scroll height
-        #     last_height = driver.execute_script("return document.body.scrollHeight")
-        #     # Scroll down to bottom
-        #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        #     # Lets hop on to the next page
-        #     more_button = find_element_click(driver, By.CSS_SELECTOR, button_selector)
-        #     print(more_button)
-
-        #     # Wait to load page
-        #     time.sleep(timeout)
-
-        #     # Calculate new scroll height and compare with last scroll height
-        #     new_height = driver.execute_script("return document.body.scrollHeight")
-        #     if new_height == last_height:
-        #         break
-        #     last_height = new_height
+        pagination = False
+        if pagination:
+            while check_exists(driver, button_selector):  
+                # Get scroll height
+                last_height = driver.execute_script("return document.body.scrollHeight")
+                # Scroll down to bottom
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # Lets hop on to the next page
+                more_button = find_element_click(driver, By.CSS_SELECTOR, button_selector)
+                # Wait to load page
+                time.sleep(timeout)
+                # Calculate new scroll height and compare with last scroll height
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break
+                last_height = new_height
             
-        # print(driver.current_url)
+        print(driver.current_url)
 
         # At this point, the preconditions are:
         # 1. we reached the max page, and 
@@ -123,30 +181,46 @@ def main():
         for e in elem_list:
             # first, get the model name
             watch_name = e.find_element(By.CLASS_NAME, '_info').get_attribute('innerHTML')
-            # then open up the watch, and scrape the info            
-            divTag = BeautifulSoup(watch_name, 'html.parser').find_all("div")
-            model_name, price = divTag[0].text, divTag[1].text
+            divTag = BeautifulSoup(watch_name, 'html.parser').find_all("div")            
+            model_name = divTag[0].text
+            if len(divTag) > 1:
+                price = divTag[1].text
 
-            ActionChains(driver) \
-                .key_down(Keys.CONTROL) \
-                .click(e) \
-                .perform()
-            driver.switch_to.window(driver.window_handles[1])
+            ActionChains(driver).key_down(Keys.CONTROL).click(e).perform()  # open up the watch page in a new tab
+            driver.switch_to.window(driver.window_handles[1])               # view new tab
+            watch_img_src = driver.find_element(By.CSS_SELECTOR, img_selector).get_attribute("src")  # select the image tag            
+            tmp_pic_file = urllib.request.urlretrieve(watch_img_src, "tmpimg.png")
+            pic_blob = convertToBinaryData("tmpimg.png")            
+            
             watch_spec_html = driver.find_element(By.CLASS_NAME, 'productSpec-items').get_attribute('innerHTML')
-            soup = BeautifulSoup(watch_spec_html, 'html.parser')
-            # handle each seiko spec element here
-            watch_info[model_name] = handle_spec_element(soup, price)
-            df = pd.DataFrame.from_dict(watch_info)
-            print(df.to_string())
-
+            soup = BeautifulSoup(watch_spec_html, 'html.parser')             # parse seiko spec element here
+            watch_info[model_name] = handle_spec_element(soup, price, pic_blob)
+            print_watch_info(watch_info[model_name])            
             driver.close()
             driver.switch_to.window(driver.window_handles[0])
             break
     except TimeoutException:
         print("Timed out waiting for list div to load")
-
+    
+    df = pd.DataFrame.from_dict(watch_info)
+    df.to_csv('seiko.csv')
     driver.close()
 
 
+
+    
+    # # populate info into sqlite3
+    # conn = sqlite3.connect('watches.db')
+    # sql = ''' INSERT INTO watches (
+    #         brand, model, weight, water_resistance, lug_to_lug, 
+    #         jewel_count, diameter, thickness, lug_width, crystal, 
+    #         lume, case_material, power_reserve, mrsp, movement_name, 
+    #         movement_type)
+    #     VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) '''
+    # cur = conn.cursor()
+    # cur.execute(sql, insert_data)
+    # conn.commit()
+
 if __name__ == '__main__':
+    init_db()
     main()
